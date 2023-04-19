0f2f7adfa3eff4e6dd937342d652789a34eb648b
CheatSetha <cheatsetha2001@gmail.com>
2023-04-19 19:20:40 +0700

    update hw-3

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import ProductCard from './ProductCard'
+import React, { useEffect, useState } from "react"
+import ProductCard from "./ProductCard"
+import SkeletonBtn from "./SkeletonBtn"
 
 const Products = () => {
-    const [products, setProducts] = useState([{}])
-
+	const [products, setProducts] = useState([{}])
+	const [loader, setLoader] = useState(false)
 	useEffect(() => {
-		fetch("https://api.escuelajs.co/api/v1/products")
-			.then((res) => res.json())
-			.then((data) => {
-				setProducts(data)
-			})
-            console.log(products);
-	})
-  return (
-    <div className='d-flex flex-wrap container '>
-        {
-            products.map(p => 
-                <ProductCard product={p}/>
-                )
-        }
-    </div>
-  )
+		setLoader(true)
+		setTimeout(async () => {
+			const response = await fetch("https://api.escuelajs.co/api/v1/products")
+			const data = await response.json()
+			setLoader(false)
+			setProducts(data)
+		}, 4000)
+	}, [])
+	return (
+		<div className='d-flex flex-wrap container justify-content-center'>
+			{!loader ? (
+				products.map((p) => <ProductCard product={p} />)
+			) : [1,2,3,4,5,6,7,8].map(loader => 	<SkeletonBtn />)	}
+			
+		
+		</div>
+	)
 }
 
 export default Products
